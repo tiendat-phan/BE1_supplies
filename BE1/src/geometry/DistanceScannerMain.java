11eda621cbeffbffe728ab63ec27dee9b1bd50be
Phan TIen Dat <phantiendat_t62@hus.edu.vn>
2022-10-05 11:26:47 +0200

    modified DistanceScannerMain.java

--- BE1/src/geometry/DistanceScannerMain.java
+++ BE1/src/geometry/DistanceScannerMain.java
@@ -5,23 +5,24 @@ import java.util.Scanner;
 
 public class DistanceScannerMain {
     public static void main(String[] args){
-        // Position p1 = new Position(2,3);
-        // Position p2 = new Position(-1,-1);
-        // // System.out.println("Distance euclide between p1 and p2: " + Position.euclide(p1,p2));
-        // // System.out.println("Distance manhattan between p1 and p2: " + Position.manhattan(p1,p2));
-
         
-        // System.out.println(Position.distance(DistanceType.MANHATTAN,p1,p2));
-        // System.out.println(Position.distance(DistanceType.EUCLIDE,p1,p2));
 
-        Scanner scan = new Scanner("Position (x,y): ");
-        System.out.println("Scanner String: \n"
-                           + scan.nextLine());
-
+        Scanner scan = new Scanner(System.in);
+        
         scan.useLocale(Locale.US);
 
-        System.out.println("Local: " + scan.locale());
+        System.out.println("Input the P1 position (x y): ");
+        
+        double x1 = scan.nextDouble();
+        double y1 = scan.nextDouble();
+        Position p1 = new Position(x1,y1);
+        System.out.println("Input the P2 position (x y): ");
+        double x2 = scan.nextDouble();
+        double y2 = scan.nextDouble();
+        Position p2 = new Position(x2,y2);
 
+        System.out.println("Distance Euclide between P1 and P2: " + Position.distance(DistanceType.EUCLIDE, p1, p2));
+        System.out.println("Distance Manhattan between P1 and P2: " + Position.distance(DistanceType.MANHATTAN, p1, p2));
         scan.close();
 
     }
